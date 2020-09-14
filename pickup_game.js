e657c69974f86594e36d1ba597030ab806aebbb8
Kevponce <Kevponce194@gmail.com>
2020-09-14 16:10:25 -0400

    add array and random generator

--- pickup_game.js
+++ pickup_game.js
@@ -1,2 +1,52 @@
-pickup_game.js
+// pickup_game.js
 
+// generate a random number
+let getRandNum = function (start, range) {
+    let getRand = Math.floor((Math.random() * range) + start);
+    while (getRand > range) {
+        getRand = Math.floor((Math.random() * range) + start);
+    };
+    return getRand;
+};
+
+// array of 10 kids
+var kids = [{
+    name: "Bobby",
+    random: getRandNum(1, 10)
+},
+{
+    name: "Frankie",
+    random: getRandNum(1, 10)
+},
+{
+    name: "Juan",
+    random: getRandNum(1, 10)
+},
+{
+    name: "Sid",
+    random: getRandNum(1, 10)
+},
+{
+    name: "Ellie",
+    random: getRandNum(1, 10)
+},
+{
+    name: "Harry",
+    random: getRandNum(1, 10)
+},
+{
+    name: "Chester",
+    random: getRandNum(1, 10)
+},
+{
+    name: "Lucio",
+    random: getRandNum(1, 10)
+},
+{
+    name: "Nancy",
+    random: getRandNum(1, 10)
+},
+{
+    name: "Kim",
+    random: getRandNum(1, 10)
+}]
